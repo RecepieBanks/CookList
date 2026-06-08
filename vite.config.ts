40bf1d7a82a8fa293d4e9d13c998c8b455958c73
RecepieBanks <odintoimmo@gmail.com>
2026-06-08 15:04:38 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   optimizeDeps: {
     include: ['react', 'react-dom', 'react/jsx-runtime', 'framer-motion'],
   },
+  
   server: {
     port: 3000,
     strictPort: true,
